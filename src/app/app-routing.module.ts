bf815a98a9bd41427ff57be2efeb21e650d22d53
rahul <bavaliya.r@iitgn.ac.in>
2023-04-28 11:05:19 +0530

    changes on main file

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { AppComponent } from "./app.component";
+ 
+const routes: Routes = [
+   
+  {
+    path: 'hello',
+    component: AppComponent,
+    
+  }
 
-const routes: Routes = [];
-
+];
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [],
+  exports: []
 })
 export class AppRoutingModule { }
